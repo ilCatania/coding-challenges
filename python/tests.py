5b2c91790d456ea27fc296c34c644180bc35b59a
Gabriele Catania <gabriele.ctn@gmail.com>
2017-11-13 13:22:57 +0000

    full test suite added

--- python/tests.py
+++ python/tests.py
@@ -2,6 +2,7 @@ import unittest
 from collections import Counter
 from random_numbers import RandomGen
 
+
 class RandomGenTest(unittest.TestCase):
 
     # the number of calls to be made to next_num()
@@ -17,47 +18,66 @@ class RandomGenTest(unittest.TestCase):
 
     def run_test(self, numbers, probabilities):
         """
-        sets up a random generator with the provided arguments, then
-        generates a significant amount of random numbers and checks
-        that the observed probabilities are close enough to the
-        expected ones.
+        sets up a random generator with the provided arguments,
+        then generates a significant amount of random numbers and checks
+        that the observed probabilities are close enough to the expected ones.
         """
         rnd_gen = RandomGen(numbers, probabilities, self.SEED)
         counts = Counter([rnd_gen.next_num() for _ in range(self.ITERATIONS)])
-        observed_probabilities = {n: c / self.ITERATIONS for n, c in counts.items()}
+        observed_probabilities =\
+            {n: (c / self.ITERATIONS) for n,c in counts.items()}
         expected_probabilities = dict(zip(numbers, probabilities))
         self.compare(observed_probabilities, expected_probabilities)
 
     def compare(self, observed, expected):
         """
-        compares two dictionaries expecting them to have the same
-        key set and values that differ by at most the configured
-        delta
+        compares two dictionaries expecting them to have the same key set
+        and values that differ by at most the configured delta
         """
         self.assertEqual(len(observed), len(expected))
-        for key, expected in expected.items():
-            self.assertAlmostEqual(expected, observed[key], delta=self.DELTA)
+        for key, expected_value in expected.items():
+            self.assertAlmostEqual(
+                expected_value, observed[key], delta=self.DELTA)
+
+    def test_too_many_probabilities(self):
+        numbers = [3.14, 2.71]
+        probabilities = [.5, .3, .2]
+        with self.assertRaises(ValueError):
+            RandomGen(numbers, probabilities)
+
+    def test_too_few_probabilities(self):
+        numbers = [3.14, 2.71, 1]
+        probabilities = [.7, .3]
+        with self.assertRaises(ValueError):
+            RandomGen(numbers, probabilities)
+
+    def test_missing_arguments(self):
+        numbers = [3.14, 2.71, 1]
+        probabilities = [.5, .3, .2]
+        with self.assertRaises(ValueError):
+            RandomGen(numbers, [])
+        with self.assertRaises(ValueError):
+            RandomGen([], probabilities)
 
     def test_probabilities_dont_add_to_one(self):
-        # assuming input probabilities need to add up to 1 makes
-        # mathematical sense and makes our life easier too
-        # TODO implement
-        pass
-
-    def test_invalid_arguments(self):
-        # TODO implement
-        pass
+        """
+        assuming input probabilities need to add up to 1 makes mathematical
+        sense and makes our life easier too
+        """
+        numbers = [3.14, 2.71]
+        probabilities = [.5, .6]
+        with self.assertRaises(ValueError):
+            RandomGen(numbers, probabilities)
 
     def test_single_number(self):
         self.run_test([3.14], [1])
 
     def test_simple_split(self):
-        # TODO implement
-        pass
+        self.run_test([3.14, 2.71], [.5, .5])
 
     def test_non_trivial_example(self):
-        # TODO implement
-        pass
+        self.run_test([-1, 0, 1, 2, 3], [.01, .3, .58, .1, .01])
+
 
 if __name__ == '__main__':
     unittest.main()
