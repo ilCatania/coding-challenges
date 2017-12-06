5044d68db2af45972a3ffac2587935076052ddd3
Gabriele Catania <gabriele.ctn@gmail.com>
2017-12-06 00:25:07 +0000

    update license headers

--- number-in-words/src/test/groovy/it/gcatania/hackajob/numberinwords/NumberInWordsSpec.groovy
+++ number-in-words/src/test/groovy/it/gcatania/hackajob/numberinwords/NumberInWordsSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Gabriele Catania <gabriele.ctn@gmail.com>
+ * Copyright 2017 Gabriele Catania <gabriele.ctn@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
